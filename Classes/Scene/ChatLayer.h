a2b49141fb23b33cfc130d4c34f7659a8c66e2b5
L-R-C-C-M <liuruimeng291@163.com>
2021-06-20 16:09:20 +0800

    update actor and chatplayer

--- Classes/Scene/ChatLayer.h
+++ Classes/Scene/ChatLayer.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef __CHAT_LAYER_H__
 #define __CHAT_LAYER_H__
 #include"Const/Const.h"
@@ -11,18 +10,4 @@ public:
 	virtual bool init();
 	void sendMessage();
 };
-=======
-#ifndef __CHAT_LAYER_H__
-#define __CHAT_LAYER_H__
-#include"Const/Const.h"
-#include "ui/CocosGUI.h"
-USING_NS_CC;
-class ChatLayer :public cocos2d::Layer
-{
-public:
-	static ChatLayer* create();
-	virtual bool init();
-	void sendMessage();
-};
->>>>>>> main
 #endif
